ac741cc2387113180ba4a08f0aa74ebf42bf2446
elias-291356 <ikucher.002@gmail.com>
2023-10-08 16:11:47 +0400

    хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,92 +1,69 @@
 import 'bulma/css/bulma.css';
-import React, { Component } from 'react';
-
-
-
+import React, { useEffect, useState } from 'react';
 import { Filter } from './Filter/Filter';
 import { Contacts } from './Contacts/Contacts';
 import { PhonebookItem } from './PhonebookItem/PhonebookItem';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     const stringifiedContacts = localStorage.getItem('key');
     const parsedContacts = JSON.parse(stringifiedContacts) ?? [];
-    this.setState({
-      contacts: parsedContacts
-    });
-  }
+    setContacts(parsedContacts);
+  }, []);
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      const stringifiedContacts = JSON.stringify(this.state.contacts);
-      localStorage.setItem('key', stringifiedContacts);
-    }
-  }
+  useEffect(() => {
+    const stringifiedContacts = JSON.stringify(contacts);
+    localStorage.setItem('key', stringifiedContacts);
+  }, [contacts]);
 
-
-  handleAddContact = (newContact) => {
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, newContact],
-      name: '',
-      number: '',
-    }));
+  const handleAddContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
   };
 
-
-
-  onDeleteContact = (id) => {
-    this.setState({
-      contacts: this.state.contacts.filter((contact) => contact.id !== id),
-    });
-  };
-
-  onFilterContact = (event) => {
-    const inputFilterValue = event.target.value;
-    this.setState({ filter: inputFilterValue });
-  };
-
-  render() {
-    const filtered = this.state.contacts.filter((contact) => {
-      return (
-        contact.name
-          .toLowerCase()
-          .includes(this.state.filter.trim().toLowerCase()) ||
-        contact.number.includes(this.state.filter)
-      );
-    });
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <div className='box '>
-          <PhonebookItem
-            name={this.state.name}
-            number={this.state.number}
-            onAddContact={this.handleAddContact}
-            contacts={this.state.contacts}
-          />
-          <Filter
-            onChange={this.onFilterContact}
-            filterList={filtered} />
-          <Contacts
-            contacts={filtered}
-            onDeleteContact={this.onDeleteContact} />
-        </div >
-      </div >
+  const onDeleteContact = (id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
     );
-  }
-}
+  };
+
+  const onFilterContact = (event) => {
+    const inputFilterValue = event.target.value;
+    setFilter(inputFilterValue);
+  };
+
+  const filtered = contacts.filter((contact) => {
+    return (
+      contact.name.toLowerCase().includes(filter.trim().toLowerCase()) ||
+      contact.number.includes(filter)
+    );
+  });
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <div className="box">
+        <PhonebookItem
+          onAddContact={handleAddContact}
+          contacts={contacts}
+        />
+        <Filter
+          onChange={onFilterContact}
+          filterList={filtered} />
+        <Contacts
+          contacts={filtered}
+          onDeleteContact={onDeleteContact} />
+      </div>
+    </div>
+  );
+};
--- src/components/PhonebookItem/PhonebookItem.jsx
+++ src/components/PhonebookItem/PhonebookItem.jsx
@@ -1,76 +1,68 @@
 import Notiflix from 'notiflix';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { nanoid } from 'nanoid';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
+export const PhonebookItem = ({ contacts, onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-export class PhonebookItem extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-
-  onInputChange = (event) => {
+  const onInputChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { name, number } = this.state;
-
-    if (this.props.contacts.some((contact) => contact.name === name)) {
-      Notiflix.Notify.warning(`A contact named "${name}" already exists.`);
-    } else {
-      const newContact = {
-        name: name,
-        number: number,
-        id: nanoid(),
-      };
-
-      this.props.onAddContact(newContact);
-
-      this.setState({
-        name: '',
-        number: '',
-      });
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
     }
   };
 
-  render() {
-    const { name, number } = this.state;
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    return (
-      <form onSubmit={this.handleSubmit} >
-        <h2 className="subtitle is-1">Phonebook</h2>
+    if (contacts.some((contact) => contact.name === name)) {
+      Notiflix.Notify.warning(`A contact named "${name}" already exists.`);
+    } else {
+      const newContact = {
+        name,
+        number,
+        id: nanoid(),
+      };
 
-        <label className="label">Name</label>
-        <input
-          placeholder="Your name"
-          className="input is-success"
-          type="text"
-          name="name"
-          maxLength="20"
-          required
-          value={name}
-          onChange={this.onInputChange}
-        />
-        <label className="label">Number</label>
-        <input
-          placeholder="+380"
-          className="input is-success"
-          type="number"
-          name="number"
-          maxLength="20"
-          required
-          value={number}
-          onChange={this.onInputChange}
-        />
-        <button className="button is-primary" type="submit" onClick={this.handleSubmit}>
-          Add contact
-        </button>
-      </form>
+      onAddContact(newContact);
 
-    );
-  }
-}
+      setName('');
+      setNumber('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="subtitle is-1">Phonebook</h2>
+
+      <label className="label">Name</label>
+      <input
+        placeholder="Your name"
+        className="input is-success"
+        type="text"
+        name="name"
+        maxLength="20"
+        required
+        value={name}
+        onChange={onInputChange}
+      />
+      <label className="label">Number</label>
+      <input
+        placeholder="+380"
+        className="input is-success"
+        type="number"
+        name="number"
+        maxLength="20"
+        required
+        value={number}
+        onChange={onInputChange}
+      />
+      <button className="button is-primary" type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
